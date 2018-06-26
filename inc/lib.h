aa3bb48cc208eaa8dd187404a847a7234f27a155
AJIOB <aleksandr9809@gmail.com>
2018-06-26 17:19:52 +0300

    Baudrate convertion fix. Read with timeout was implemented

--- inc/lib.h
+++ inc/lib.h
@@ -9,7 +9,23 @@ typedef TTY_Handle_t* tty_handler_t;
 err_t parse_args(int argc, char** argv, struct args_t* hargs);
 err_t open_serial(struct args_t* args, tty_handler_t* hserial);
 err_t send_info(struct args_t* args, tty_handler_t* hserial);
+err_t send_info_file(struct args_t* args, tty_handler_t* hserial);
 err_t receive_info(struct args_t* args, tty_handler_t* hserial);
 err_t close_serial(struct args_t* args, tty_handler_t* hserial);
 
-int tty_convert_baudrate (unsigned long long baudrate);
+/**
+ * @brief Convert baudrate from human-readable to special-defined
+ * 
+ * @param baudrate Human-readable baudrate
+ * @return int Special-defined baudrate
+ */
+int tty_convert_baudrate(unsigned long long baudrate);
+
+/**
+ * @brief Calculate stop time value
+ * 
+ * @param delta Time in seconds from now to stop time
+ * @param res Ponter to time structure
+ * @return err_t Error status
+ */
+err_t time_get_stop_time(float delta, struct timeval* res);
--- src/lib.c
+++ src/lib.c
@@ -1,5 +1,14 @@
 #include <string.h>
 
+// for receive_info
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/select.h>
+
+// for timers
+#include <sys/time.h>
+
 #include "lib.h"
 
 err_t parse_args(int argc, char** argv, struct args_t* hargs)
@@ -52,6 +61,19 @@ err_t open_serial(struct args_t* args, tty_handler_t* hserial)
 
 err_t send_info(struct args_t* args, tty_handler_t* hserial)
 {
+    switch (args->program_mode)
+    {
+    case none:
+        return err_no;
+    case file:
+        return send_info_file(args, hserial);
+    default:
+        return err_not_implemented;
+    }
+}
+
+err_t send_info_file(struct args_t* args, tty_handler_t* hserial)
+{
     if (args->program_mode != file)
     {
         return err_not_implemented;
@@ -91,9 +113,52 @@ err_t send_info(struct args_t* args, tty_handler_t* hserial)
 
 err_t receive_info(struct args_t* args, tty_handler_t* hserial)
 {
-    //TODO
+    fd_set set;
+    int filedesc = (*hserial)->fileDes;
+    int err_int;
+    err_t err;
 
-    return err_not_implemented;
+    FD_ZERO(&set); /* clear the set */
+    FD_SET(filedesc, &set); /* add our file descriptor to the set */
+
+    /* timeout block */
+    struct timeval t_stop;
+    err = time_get_stop_time(args->recv_time, &t_stop);
+    err_check(err);
+
+    while (1)
+    {
+        struct timeval t_curr;
+        err_int = gettimeofday(&t_curr, NULL);
+        err_check(err_not_equal(err_int, 0));
+
+        struct timeval timeout;
+        timersub(&t_curr, &t_stop, &timeout);
+
+        int rv;
+        rv = select(filedesc + 1, &set, NULL, NULL, &timeout);
+        if(rv == -1)
+        {
+            //error
+            err_check(err_yes);
+        }
+        else if(rv == 0)
+        {
+            //timeout
+            break;
+        }
+        else
+        {
+            /* there was data to read */
+            err_t err;
+            char buff;
+            err = TTY_Read(*hserial, (uint8_t *)&buff, 1);
+            err_check(err);
+
+            putchar(buff);
+        }
+    }
+    return err_no;
 }
 
 err_t close_serial(struct args_t* args, tty_handler_t* hserial)
@@ -105,27 +170,62 @@ err_t close_serial(struct args_t* args, tty_handler_t* hserial)
     return err_no;
 }
 
-int tty_convert_baudrate (unsigned long long baudrate)
+int tty_convert_baudrate(unsigned long long baudrate)
 {
     int endcode = 0;
 
     switch(baudrate){
-        case 50:{       endcode = B200;     break;}
-        case 75:{       endcode = B300;     break;}
-        case 150:{      endcode = B600;     break;}
-        case 300:{      endcode = B1200;    break;}
-        case 600:{      endcode = B2400;    break;}
-        case 1200:{     endcode = B4800;    break;}
-        case 2400:{     endcode = B9600;    break;}
-        case 4800:{     endcode = B19200;   break;}
-        case 9600:{     endcode = B38400;   break;}
-        case 57600:{    endcode = B230400;  break;}
-        case 115200:{   endcode = B460800;  break;}
-        case 230400:{   endcode = B921600;  break;}
-        case 500000:{   endcode = B2000000; break;}
-        case 1000000:{  endcode = B4000000; break;}
+        case 50:{       endcode = B50;      break;}
+        case 75:{       endcode = B75;      break;}
+        case 110:{      endcode = B110;     break;}
+        case 134:{      endcode = B134;     break;}
+        case 150:{      endcode = B150;     break;}
+        case 200:{      endcode = B200;     break;}
+        case 300:{      endcode = B300;     break;}
+        case 600:{      endcode = B600;     break;}
+        case 1200:{     endcode = B1200;    break;}
+        case 1800:{     endcode = B1800;    break;}
+        case 2400:{     endcode = B2400;    break;}
+        case 4800:{     endcode = B4800;    break;}
+        case 9600:{     endcode = B9600;    break;}
+        case 19200:{    endcode = B19200;   break;}
+        case 38400:{    endcode = B38400;   break;}
+        case 57600:{    endcode = B57600;   break;}
+        case 115200:{   endcode = B115200;  break;}
+        case 230400:{   endcode = B230400;  break;}
+        case 460800:{   endcode = B460800;  break;}
+        case 500000:{   endcode = B500000;  break;}
+        case 576000:{   endcode = B576000;  break;}
+        case 921600:{   endcode = B921600;  break;}
+        case 1000000:{  endcode = B1000000; break;}
+        case 1152000:{  endcode = B1152000; break;}
+        case 1500000:{  endcode = B1500000; break;}
+        case 2000000:{  endcode = B2000000; break;}
+        case 2500000:{  endcode = B2500000; break;}
+        case 3000000:{  endcode = B3000000; break;}
+        case 3500000:{  endcode = B3500000; break;}
+        case 4000000:{  endcode = B4000000; break;}
         default:{       endcode = B0;       break;}
     }
 
     return endcode;
+}
+
+err_t time_get_stop_time(float delta, struct timeval* res)
+{
+    int err_int;
+    struct timeval t_start, t_delta;
+
+    // start time
+    err_int = gettimeofday(&t_start, NULL);
+    err_check(err_not_equal(err_int, 0));
+
+    //calc new time
+    long long seconds = (long long)(delta);
+    t_delta.tv_sec = (time_t)seconds;
+    t_delta.tv_usec = (time_t)((delta - seconds) * 1000000);
+
+    timeradd(&t_start, &t_delta, res);
+
+    return err_no;
 }
